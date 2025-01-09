cbc59444bd9b3c9da88956dfd19bcd6bcf981aeb
Pk2414 <prasanna0022192@gmail.com>
2025-01-09 12:28:50 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,13 +1,5 @@
-a=10
-b=12
-c=13
-if a>b: 
-    if a>c:
-        print("first number is bigger")
-    else:
-        print("third nuber is bigger")
-else:
-    if b>c:
-        print("second number is bigger")
-    else:
-        print("third nuber is bigger")
+l=[]
+for i in range (1,100):
+    if i%2==0:
+        l.append(i)
+print(l)
