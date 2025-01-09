75d72be0232317cbe1caa1a8b6dc5f1a14561db0
Pk2414 <prasanna0022192@gmail.com>
2025-01-09 12:31:40 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,5 +1,20 @@
-l=[]
-for i in range (1,100):
-    if i%2==0:
-        l.append(i)
-print(l)
+low, high = 1, 100
+primes = []
+for i in range(low, high + 1):
+    flag = 0
+
+    if i < 2:
+        continue
+    if i == 2:
+        primes.append(2)
+        continue
+
+    for x in range(2, i):
+        if i % x == 0:
+            flag = 1
+            break
+
+    if flag == 0:
+        primes.append(i)
+        
+print(primes)
