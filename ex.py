a43fc88a3b15a2eaa7197355c74d0aee0a8bf427
Pk2414 <prasanna0022192@gmail.com>
2025-01-09 12:20:24 +0530

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,13 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+if a>b: 
+    if a>c:
+        print("first number is bigger")
+    else:
+        print("third nuber is bigger")
+else:
+    if b>c:
+        print("second number is bigger")
+    else:
+        print("third nuber is bigger")
